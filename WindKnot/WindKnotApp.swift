0d3bd93a9a6f7bcef26759563d1234e661b8a30f
Emre <emre.degirmenci@sisal.com>
2024-10-03 22:58:07 +0300

    Initial Commit

---
+++ WindKnot/WindKnotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindKnotApp.swift
+//  WindKnot
+//
+//  Created by emre.degirmenci on 3.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct WindKnotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
